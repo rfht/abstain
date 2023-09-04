623c1d3ec16cc3736a679107ce130f2ecd089368
thfrwn <11335318+rfht@users.noreply.github.com>
2023-09-04 15:52:08 -0400

    fix use of is_string_in_array; some cleanup; add LICENSE file

--- abstain.c
+++ abstain.c
@@ -25,6 +25,9 @@
 #define MAX_PROMISE_LENGTH	16	/* max string length of promises */
 #define MAX_VICES		64
 
+#define OK			0
+#define FAIL			-1
+
 /* don't include error promise here because it is pledged by default */
 char *promise_all[] = {
 	"stdio", "rpath", "wpath", "cpath",
@@ -53,46 +56,38 @@ void usage(char *self) {
 }
 
 int is_string_in_array(char *str, size_t max_str, char **arr, size_t arr_len) {
-	for (int i = 0; i < arr_len; i++) {
-		printf("DEBUG: str: %s, arr[%d]: %s\n", str, i, arr[i]);
+	for (int i = 0; arr[i] != NULL && i < arr_len; i++) {
 		if (strncmp(str, arr[i], max_str) == 0)
-			return 0;
+			return OK;
 	}
-	return -1;
+	return FAIL;
 }
+
+//int path_lookup(char *bin, char *pathenv) {
+//}
 
 void run(int argc, char **argv) {
 	char *promise;
 	char *executable;
+	char *full_executable;
 	char *argstring = NULL;
 
-	/*
-	while (argc-- > 0) {
-		if (argstring == NULL)
-			argstring = *argv;
-		else {
-			if (snprintf(argstring, STR_MAX, "%s %s", argstring, *argv++) < 0)
-				err(1, NULL);
-		}
-		argv++;
-	}
-	*/
-
 	for (int i = 0; i < nvices; i++) {
-		if (is_string_in_array(vices[i], MAX_PROMISE_LENGTH, promise_all, sizeof(promise_all)) == -1) {
-			err(1, "invalid vice: %s", vices[i]);
+		if (is_string_in_array(vices[i], MAX_PROMISE_LENGTH, promise_all,
+		                       (sizeof(promise_all) / sizeof(promise_all[0]) - 1)) == FAIL) {
+			errx(1, "invalid vice: %s", vices[i]);
 		}
 	}
 
 	if ((execpromises = malloc(STR_MAX)) == NULL)
-		err(1, NULL);
+		errx(1, NULL);
 	if (strlcpy(execpromises, promise_error, sizeof(execpromises)) > sizeof(execpromises))
-		err(1, NULL);
+		errx(1, NULL);
 
 	for (int i = 1; (promise = promise_all[i]) != NULL; i++) {
-		if (is_string_in_array(promise, sizeof(promise), vices, MAX_PROMISE_LENGTH) != -1) {
+		if (is_string_in_array(promise, sizeof(promise), vices, MAX_PROMISE_LENGTH) == FAIL) {
 			if (snprintf(execpromises, STR_MAX, "%s %s", execpromises, promise) < 0)
-				err(1, NULL);
+				errx(1, NULL);
 		}
 	}
 	if (execpromises[0] == ' ') {
@@ -101,6 +96,12 @@ void run(int argc, char **argv) {
 
 	executable = *argv++;
 	argc--;
+
+	/* get absolute path of executable */
+	if(access(executable, X_OK) == 0)
+		printf("%s is executable\n", executable);
+	else
+		printf("%s is not an executable file\n", executable);
 
 	printf("executing `%s' ", executable);
 	if (argc > 0) {
@@ -111,9 +112,9 @@ void run(int argc, char **argv) {
 	}
 	printf("with the following execpromises: \%s\n", execpromises);
 	if (pledge(NULL, execpromises) == -1)
-		err(1, NULL);
+		errx(1, NULL);
 	//if (execve ... == -1)
-		//    err(1, NULL);
+		//    errx(1, NULL);
 
 	exit(0);
 }
@@ -123,7 +124,7 @@ int main(int argc, char** argv) {
 	char *self = argv[0];
 
 	if ((vices = calloc(MAX_VICES, MAX_PROMISE_LENGTH)) == NULL)
-	    err(1, NULL);
+	    errx(1, NULL);
 
 	while ((ch = getopt_long(argc, argv, "v:", longopts, NULL)) != -1) {
 		switch (ch) {
